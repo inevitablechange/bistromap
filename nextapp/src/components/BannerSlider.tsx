0f894d215659a25fdbe2aa57fa12c7696cfe7439
YeonhaPark <yonaprisca@gmail.com>
2024-08-21 00:05:18 +0900

    feat: banner 민트 기능

--- nextapp/src/components/BannerSlider.tsx
+++ nextapp/src/components/BannerSlider.tsx
@@ -56,7 +56,7 @@ const BannerSlider: React.FC<BannerSliderProps> = ({ setIsModalOpen }) => {
       navigation
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
+      // onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log("slide change")}
     >
       {nftBanners.map((banner) => (
