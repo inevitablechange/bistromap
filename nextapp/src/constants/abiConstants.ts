db4af318abc4dc00132d54053ac987bb2f018b5e
inevitablechange <yongho7783@gmail.com>
2024-08-22 14:51:16 +0900

    feat: Add Swap page

---
+++ nextapp/src/constants/abiConstants.ts
@@ -0,0 +1,12 @@
+export const ROUTER_ABI = [
+  "function WETH() external pure returns (address)",
+  "function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts)",
+  "function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)",
+  "function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)",
+  "function swapExactTokensForTokens(uint256 amountIn, uint256 amountOutMin, address[] calldata path, address to, uint256 deadline) external returns (uint256[] memory amounts)",
+];
+
+export const ERC20_ABI = [
+  "function approve(address spender, uint256 amount) public returns (bool)",
+  "function allowance(address owner, address spender) public view returns (uint256)",
+];
